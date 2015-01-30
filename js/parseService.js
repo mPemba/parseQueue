878d327fd6af386738793848eee5b55f4640c46d
Michael Sayer <michael.j.say@gmail.com>
2015-01-30 10:15:57 -0700

    still working

--- js/parseService.js
+++ js/parseService.js
@@ -3,6 +3,7 @@ var app = angular.module('parseQ');
 app.service('parseService', function($http){
 
 	this.postData = function(question) {
+		var url = "https://api.parse.com/1/classes/question'"
 		return $http({
 			method: 'POST',
 			url: 'https://api.parse.com/1/classes/question',
@@ -16,5 +17,29 @@ app.service('parseService', function($http){
     		url: 'https://api.parse.com/1/classes/question/',
     	})
     }
+///////////////////////////////////////notes or other ways
+
+
+// this.postQ = function(question) {
+//   var data = {};
+//   data.question = question;
+//   data.status = 'red';
+//   return$http.post(url, data);
+// }
+
+// this.getQ = function() {
+// 	var dfd = $q.defer();
+// 	$http.get(url + "?order=createdAt").then(function(data) {
+// 		dfd.resolve(data.data.results);
+// 	})
+// 	return dfd.promise;
+// }
+// this.escalate = function(questionObj) {
+// 	questionObj.status = "yellow";
+// 	return $http.put(url + '/' + questionObj.objectId, {status: questionObj.status});
+// }
+// this.del = function(questionObj) {
+//     return $http.delete(url + '/' + questionObj.objectId)
+// }
 
 });
